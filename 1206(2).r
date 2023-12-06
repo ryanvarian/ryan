bde0255eb66211dd75d1c3371f29a1c25fc24afc
ryanvarian <144878781+ryanvarian@users.noreply.github.com>
2023-12-06 13:43:05 +0800

    Add files via upload

---
+++ 1206(2).r
@@ -0,0 +1,32 @@
+{
+  x + 3
+}
+
+value_x <- c(2, 4, 7, 15)
+seq_along(value_x)
+for(x in seq_along(value_x))
+{
+  values_x[[x]]+3
+}
+
+myfun <- function()
+  {
+  x+3
+}
+
+
+x = 17
+myfun()
+
+#Task by situation
+
+oneComingStudentMajor <- sample(c("Economics", "Sociology"), 1)
+
+# oneComingStudentMajor == "Economics"
+print("Please go to room 201.")
+
+# oneComingStudentMajor == "Sociology"
+print("Please go to room 301.")
+
+x = 77
+myfun()
